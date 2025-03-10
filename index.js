95d256352da5b01736166d548bc5a9d5f3a55b83
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-10 21:46:57 +0300

    learning array methods

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+ 
+
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W");
+    
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        return undefined;
+    }
+}
+
+// Example usage:
+const record = [
+    { year: "2015", result: "L" },
+    { year: "2016", result: "W" },
+    { year: "2017", result: "L" }
+];
+
+console.log(superbowlWin(record));
